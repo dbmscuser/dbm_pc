2462aa2bf2194ed6acb275d2ff3ff14031037547
dbmscuser <dbmscuser>
2025-05-14 16:38:45 +0000

    Commit2

---
+++ dbo/Tables/Creation-Scripts/Persons.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Persons] (
+    [PersonID]  INT           NULL,
+    [LastName]  VARCHAR (255) NULL,
+    [FirstName] VARCHAR (255) NULL,
+    [Address]   VARCHAR (255) NULL,
+    [City]      VARCHAR (255) NULL
+);
