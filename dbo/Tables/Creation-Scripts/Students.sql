55f2e7a1585850f42ef6a5c22c0c8a2ebc696535
dbmscuser <dbmscuser>
2025-05-14 16:39:17 +0000

    Commit3

---
+++ dbo/Tables/Creation-Scripts/Students.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Students] (
+    [id]      INT          NULL,
+    [name]    VARCHAR (50) NULL,
+    [address] TEXT         NULL,
+    [grades]  VARCHAR (50) NULL,
+    [phone]   VARCHAR (10) NULL
+);
